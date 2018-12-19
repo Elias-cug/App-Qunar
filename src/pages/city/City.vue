200cb25bab706f5f16000d0c95814769b16b4252
liyisheng <liyish@163.com>
2018-12-19 14:42:32 +0800

    add search to city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,15 +1,19 @@
 <template>
-  <city-header></city-header>
+  <div>
+    <city-header></city-header>
+    <city-search></city-search>
+  </div>
 </template>
 <script>
 import CityHeader from './components/Header.vue'
+import CitySearch from './components/Search.vue'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
 <style lang="stylus" scoped>
-
 </style>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或拼英">
+  </div>
+</template>
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .search
+    height: .72rem
+    background: $bgColor
+    padding: 0 .1rem
+    .search-input
+      box-sizing: border-box
+      width: 100%
+      height: .62rem
+      padding: 0 .1rem
+      line-height: .62rem
+      text-align: center
+      border-radius: .06rem
+      color: #666
+</style>
