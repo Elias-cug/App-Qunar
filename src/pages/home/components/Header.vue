ca175f053e14913bcc97c229c90f47de5c70b834
liyisheng <liyish@163.com>
2018-12-19 11:21:26 +0800

    city router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
         <span class="iconfont">&#xe632;</span>
         输入城市/景点/游玩主题
       </div>
-      <div class="header-right">
-        {{city}}
-        <span class="iconfont arrow-icon">&#xe64a;</span>
-      </div>
+      <router-link to="/city">
+        <div class="header-right">
+          {{city}}
+          <span class="iconfont arrow-icon">&#xe64a;</span>
+        </div>
+      </router-link>
     </div>
 </template>
 
@@ -52,6 +54,7 @@ export default {
       line-height: .86rem
       float: right
       text-align: center
+      color: #fff
       .arrow-icon
         margin-left: -.04rem
         font-size: .24rem
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../pages/home/Home.vue'
+import City from '../pages/city/City.vue'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: Home
+  }, {
+    path: '/city',
+    name: 'City',
+    component: City
+  } ]
 })
